9527343b4f86d314cb3175a7c3ced13303b38d47
Damian Kolaska <damian.kolaska99@gmail.com>
2023-04-29 21:12:05 +0200

    Move Google API client secret to SecretManager

--- main.py
+++ main.py
@@ -1,25 +1,48 @@
 import os
-import pathlib
-
 import requests
+import json
+
 from flask import Flask, session, abort, redirect, request
+from pip._vendor import cachecontrol
 from google.oauth2 import id_token
 from google_auth_oauthlib.flow import Flow
-from pip._vendor import cachecontrol
 import google.auth.transport.requests
+from google.cloud import secretmanager
 
 app = Flask("Google Login App")
 app.secret_key = "CodeSpecialist.com"
 
 os.environ["OAUTHLIB_INSECURE_TRANSPORT"] = "1"
 
-GOOGLE_CLIENT_ID = "1004315401260-sb96ghd77in8k72rukq5tbn2blaa8b5r.apps.googleusercontent.com"
-client_secrets_file = os.path.join(pathlib.Path(__file__).parent, "client_secret.json")
 
-flow = Flow.from_client_secrets_file(
-    client_secrets_file=client_secrets_file,
-    scopes=["https://www.googleapis.com/auth/userinfo.profile", "https://www.googleapis.com/auth/userinfo.email", "openid"],
-    redirect_uri="https://mystic-stack-382412.lm.r.appspot.com/callback"
+PROJECT_ID = "mystic-stack-382412"
+GOOGLE_CLIENT_ID = (
+    "1004315401260-sb96ghd77in8k72rukq5tbn2blaa8b5r.apps.googleusercontent.com"
+)
+
+
+def access_secret_version(secret_id, version_id="latest"):
+    # Create the Secret Manager client.
+    client = secretmanager.SecretManagerServiceClient()
+
+    # Build the resource name of the secret version.
+    name = f"projects/{PROJECT_ID}/secrets/{secret_id}/versions/{version_id}"
+
+    # Access the secret version.
+    response = client.access_secret_version(name=name)
+
+    # Return the decoded payload.
+    return response.payload.data.decode("UTF-8")
+
+
+client_config = json.loads(access_secret_version("GOOGLE_AUTH_CLIENT_SECRET"))
+flow = Flow.from_client_config(
+    client_config=client_config,
+    scopes=[
+        "https://www.googleapis.com/auth/userinfo.profile",
+        "https://www.googleapis.com/auth/userinfo.email",
+    ],
+    redirect_uri="https://mystic-stack-382412.lm.r.appspot.com/callback",
 )
 
 
@@ -53,9 +76,7 @@ def callback():
     token_request = google.auth.transport.requests.Request(session=cached_session)
 
     id_info = id_token.verify_oauth2_token(
-        id_token=credentials._id_token,
-        request=token_request,
-        audience=GOOGLE_CLIENT_ID
+        id_token=credentials._id_token, request=token_request, audience=GOOGLE_CLIENT_ID
     )
 
     session["google_id"] = id_info.get("sub")
@@ -77,7 +98,9 @@ def index():
 @app.route("/protected_area")
 @login_is_required
 def protected_area():
-    return f"Hello {session['name']}! <br/> <a href='/logout'><button>Logout</button></a>"
+    return (
+        f"Hello {session['name']}! <br/> <a href='/logout'><button>Logout</button></a>"
+    )
 
 
 if __name__ == "__main__":
