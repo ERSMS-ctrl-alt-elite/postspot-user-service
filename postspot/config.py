05d1d9fd15500d15842a12597da452fe19a6a21e
Damian Kolaska <damian.kolaska99@gmail.com>
2023-05-01 18:13:33 +0200

    fix8: Connect to Cloud SQL via Proxy

--- postspot/config.py
+++ postspot/config.py
@@ -17,4 +17,6 @@ class Config:
     def database_uri(self) -> str:
         if self._env == Environment.LOCAL:
             return "sqlite:///Database.db"
+        if self._env == Environment.BUILD:
+            return "mysql+pymysql://postspot:<Po~.igsUFAlYAx@127.0.0.1:3306/postspot"
         return "mysql+pymysql://postspot:<Po~.igsUFAlYAx@/postspot?unix_socket=/cloudsql/mystic-stack-382412:europe-central2:test-instance"
