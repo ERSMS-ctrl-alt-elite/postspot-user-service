a2218f5775548ec7c975d641a93bde07be8bac70
Damian Kolaska <damian.kolaska99@gmail.com>
2023-05-01 10:24:50 +0200

    Update MySQL connection string

--- postspot/config.py
+++ postspot/config.py
@@ -17,4 +17,4 @@ class Config:
     def database_uri(self) -> str:
         if self._env == Environment.LOCAL:
             return "sqlite:///Database.db"
-        return "mysql+pymysql://root:mvYHY`61kfCtB892@/mysql?unix_socket=/cloudsql/mystic-stack-382412:europe-central2:test-instance"
+        return "mysql+pymysql://postspot:<Po~.igsUFAlYAx@/postspot?unix_socket=/cloudsql/mystic-stack-382412:europe-central2:test-instance"
