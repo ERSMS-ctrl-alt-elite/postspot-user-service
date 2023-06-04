3f7889b8a1a7406c316a79058468a941d9a930de
Damian Kolaska <damian.kolaska99@gmail.com>
2023-06-04 19:33:11 +0200

    Add Swagger UI

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from datetime import datetime
 from functools import wraps
 
 from flask import Flask, request, jsonify
+from flask_swagger_ui import get_swaggerui_blueprint
 
 from postspot.data_gateway import FirestoreGateway, User
 from postspot.config import Config
@@ -18,17 +19,28 @@ env = Environment(os.environ["ENV"]) if "ENV" in os.environ else Environment.PRO
 
 config = Config(env)
 
+# ----------------------------- Configure logging ---------------------------- #
 logging.basicConfig(
     level=config.log_level,
     format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
 )
 logger = logging.getLogger(__name__)
 
+# ------------------------------- Create an app ------------------------------ #
 logger.info(f"Running application in {env.value} environment")
 app = Flask("PostSpot User Service")
 app.secret_key = "PostSpot123"
 
+# -------------------------- Create database gateway ------------------------- #
 data_gateway = FirestoreGateway()
+
+# --------------------------- Configure Swagger UI --------------------------- #
+SWAGGER_URL = "/swagger"
+API_URL = "/static/swagger.json"
+SWAGGERUI_BLUEPRINT = get_swaggerui_blueprint(
+    SWAGGER_URL, API_URL, config={"app_name": "Seans-Python-Flask-REST-Boilerplate"}
+)
+app.register_blueprint(SWAGGERUI_BLUEPRINT, url_prefix=SWAGGER_URL)
 
 
 def user_signed_up(function):
@@ -76,6 +88,11 @@ def user_signed_up(function):
 # ---------------------------------------------------------------------------- #
 #                                   Endpoints                                  #
 # ---------------------------------------------------------------------------- #
+
+
+@app.route("/")
+def index():
+    return "Hello from PostSpot's user service"
 
 
 @app.route("/signup", methods=["POST"])
