b5445d2b44603d723decf327364638f2cce4be5a
Damian Kolaska <damian.kolaska99@gmail.com>
2023-06-04 18:42:36 +0200

    Create Terraform structure for prod and dev environments

---
+++ environments/prod/providers.tf
@@ -0,0 +1,18 @@
+terraform {
+  backend "gcs" {
+    bucket = "postspot-tf-state"
+    prefix = "env/prod"
+  }
+
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "~> 4.67.0"
+    }
+  }
+}
+
+provider "google" {
+  project = var.gcp_project_id
+  region  = var.gcp_region
+}
